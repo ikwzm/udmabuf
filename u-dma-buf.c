9326764b16fd00af99dac3f396622abb3d629674
Florian Bezdeka <florian.bezdeka@siemens.com>
2020-12-04 09:49:00 +0100

    Add build support for kernel >= 5.10
    linux/dma-noncoherent.h does no longer exist and has to be replaced by
    linux/dma-map-ops.h.

--- u-dma-buf.c
+++ u-dma-buf.c
@@ -77,7 +77,11 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 #if     ((LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0)) && (defined(CONFIG_ARM) || defined(CONFIG_ARM64)))
 #if     (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
+#if     (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0))
+#include <linux/dma-map-ops.h>
+#else
 #include <linux/dma-noncoherent.h>
+#endif
 #define IS_DMA_COHERENT(dev) dev_is_dma_coherent(dev)
 #else
 #define IS_DMA_COHERENT(dev) is_device_dma_coherent(dev)
