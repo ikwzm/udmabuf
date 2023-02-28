9dc15b00177fc9692edb1d8d1bd4a8fc9af8b7e3
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-03-01 07:10:18 +0900

    [update] DRIVER_VERSION to 4.2.0

--- u-dma-buf-mgr.c
+++ u-dma-buf-mgr.c
@@ -51,7 +51,7 @@ MODULE_DESCRIPTION("U-dma-buf(User space mappable DMA buffer device driver) Mana
 MODULE_AUTHOR("ikwzm");
 MODULE_LICENSE("Dual BSD/GPL");
 
-#define DRIVER_VERSION     "4.2.0-rc3"
+#define DRIVER_VERSION     "4.2.0"
 #define DRIVER_NAME        "u-dma-buf-mgr"
 
 /**
