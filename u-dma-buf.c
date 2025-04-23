ace803ca641520fbabef9bf46f17e2e180b3b4cb
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-04-24 07:53:22 +0900

    [update] DRIVER_VERSION to 5.3.0-RC1

--- u-dma-buf.c
+++ u-dma-buf.c
@@ -66,7 +66,7 @@ MODULE_DESCRIPTION("User space mappable DMA buffer device driver");
 MODULE_AUTHOR("ikwzm");
 MODULE_LICENSE("Dual BSD/GPL");
 
-#define DRIVER_VERSION     "5.2.0"
+#define DRIVER_VERSION     "5.3.0-RC1"
 #define DRIVER_NAME        "u-dma-buf"
 #define DEVICE_NAME_FORMAT "udmabuf%d"
 #define DEVICE_MAX_NUM      256
